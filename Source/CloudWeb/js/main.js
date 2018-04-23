8098f0fbfe78f61f88eacdebf8990cf3f381568f
Cameron <camlecuyer@yahoo.com>
2018-04-23 15:53:59 -0500

    Basic webpage

--- Source/CloudWeb/js/main.js
+++ Source/CloudWeb/js/main.js
@@ -1,5 +1,15 @@
 var app = angular.module('reportResponse', []);
 
-app.controller('report-control', function($scope) {
+app.controller('login-control', function($scope, $http) {
+    $scope.login = function() {
+        /*$http({
+            url: user.details_path,
+            method: "GET",
+            params: {user_id: user.id}
+        });*/
+    };
+});
+
+app.controller('report-control', function($scope, $http) {
 
 });
