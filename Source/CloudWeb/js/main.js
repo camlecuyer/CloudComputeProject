2a8531a1b61005c5c9a9503327732df0e370fc14
Cameron <camlecuyer@yahoo.com>
2018-04-25 17:01:25 -0500

    Updated web page

--- Source/CloudWeb/js/main.js
+++ Source/CloudWeb/js/main.js
@@ -1,32 +1,49 @@
 var app = angular.module('reportResponse', []);
 
-app.controller('login-control', function($scope, $http)
+app.controller('login-control', function($scope, $http, $window)
 {
     $scope.login = function()
     {
         if ($scope.user != null && $scope.password != null)
         {
+            $.post("http://18.219.51.47/get_user.php",{
+            email: $scope.user, pass: $scope.password},
+                function(result){
+                    var res = JSON.parse(result);
 
-            $http({
+                    if(res["success"] == "1")
+                    {
+                        $window.location.href = "./main.html";
+                    }
+                    else
+                    {
+                        window.alert("Incorrect Login")
+                        console.log(res["message"]);
+                    }
+                });
+
+
+            /*$http({
                 url: "http://18.219.51.47/get_user.php",
                 method: "POST",
                 params: {
                     email: $scope.user,
                     pass: $scope.password
-                },
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }//,
+                //headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             }).then(function(response) {
-                //var res = JSON.parse(response);
+                var res = response.data;
 
-                if(response["suceess"] == "1")
+                if(res["suceess"] == "1")
                 {
-
+                    $window.location.href = "/main.html";
                 }
                 else
                 {
-                    console.log(response["message"]);
+                    window.alert("Incorrect Login")
+                    console.log(res["message"]);
                 }
-            });
+            });*/
         }
         /*
         * temp = new JSONObject(response);
@@ -40,9 +57,33 @@ app.controller('login-control', function($scope, $http)
 });
 
 app.controller('report-control', function($scope, $http) {
-    var init = function() {
+    $scope.init = function() {
+
+        $scope.getIssues();
+
+        $scope.getTasks();
+
+        $http.get("http://18.219.51.47/get_items.php").then(function(result) {
+            $scope.categories = result.data.results[1].categories;
+            $scope.buildings = result.data.results[0].buildings;
+            $scope.divisions = result.data.results[2].divisions;
+        });
 
     }
 
-    init();
+    $scope.getIssues = function() {
+        $http.get("http://18.219.51.47/get_issues.php").then(function(result) {
+            $scope.issues = result.data.results[0].issues;
+        });
+    }
+
+    $scope.getTasks = function() {
+        $http.get("http://18.219.51.47/get_tasks.php").then(function(result) {
+            $scope.tasks = result.data.results[0].tasks;
+        });
+
+        $http.get("http://18.219.51.47/get_closedtasks.php").then(function(result) {
+            $scope.closedtasks = result.data.results[0].tasks;
+        });
+    }
 });
